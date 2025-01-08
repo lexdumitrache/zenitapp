a77a840a506146c8c03736945c27636b5b03f18e
alexandra dumitrache <60842627+lexdumitrache@users.noreply.github.com>
2025-01-08 15:38:42 +0100

    Initial commit from your app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import TasksPage from './pages/TasksPage';
 import InboxPage from './pages/InboxPage';
+import ProfilePage from './pages/ProfilePage';
 import Navigation from './components/Navigation';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/tasks" element={<TasksPage />} />
           <Route path="/inbox" element={<InboxPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
         <Navigation />
       </div>
